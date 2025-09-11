ef1726c105e1824f419e4b444e2d6d74ef96779c
jurra <jose.urra86@gmail.com>
2025-09-11 15:59:56 +0200

    refactor: Update Capacitor entity to follow standard Element pattern
    - Replace legacy constructor (id, position, capacitance) with standard Element signature
    - Use proper Element base class constructor with (id, nodes, label, properties)
    - Set element type to 'capacitor' for consistency with other elements
    - Remove legacy describe() method in favor of inherited Element.describe()
    - Aligns Capacitor with Wire, Resistor, Junction, Ground element patterns
    
    Part of standardizing all circuit elements to use consistent interfaces.

--- src/domain/entities/Capacitor.js
+++ src/domain/entities/Capacitor.js
@@ -1,12 +1,8 @@
 import { Element } from "./Element.js";
 
 export class Capacitor extends Element  {
-    constructor(id, position, capacitance) {
-        super(id, position);
-        this.capacitance = capacitance; // Capacitance in Farads
-    }
-
-    describe() {
-        return `Capacitor ${this.id} at (${this.position.x}, ${this.position.y}) with capacitance ${this.capacitance}F`;
+    constructor(id, nodes, label = null, properties) {
+        super(id, nodes, label, properties);
+        this.type = 'capacitor';
     }
 }
